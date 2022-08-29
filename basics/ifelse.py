e0cac428068bc24a069fa5a1408a9bcb9472bf93
bala <ibalachandar86@gmail.com>
2022-08-29 21:51:14 +0530

    learn1

---
+++ basics/ifelse.py
@@ -0,0 +1,15 @@
+# Logical Operators
+< Less than
+> Greater than
+<= Less than equal
+>= Greater than equal
+
+a = int(input("Type a number: "))
+if a == 10:
+    print("Number is equal to 10")
+elif a == 20 | a == 30:
+    print("Number is equal to 20 or 30")
+else:
+    print("Number is not equal to given values")
+
+b = int(input("Type a number: "))
